0f4c253a5c09275bd8b556d7d4b275bafc3807dc
Michael Nutt <michael@nuttnet.net>
2013-09-24 20:24:44 -0400

    splits out config.js into its own file

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+var fs = require('fs');
+
+if(!fs.existsSync('./config.js')) {
+  console.log("Please configure config.js.example and copy it to config.js.");
+  process.exit(1);
+}
+
 var github = require('octonode')
-	, j5 = require('johnny-five');
+	, j5 = require('johnny-five')
+  , config = require('./config');
 
 
 var board = j5.Board();
@@ -7,15 +15,15 @@ var board = j5.Board();
 
 
 //add your github token
-var client = github.client('xxxxxxxx');
+var client = github.client(config.apiToken);
 
-//enter your repository, it must be a repository where you have pull rights, and it needs to have CI incorporated. 
-var repo = client.repo('username/reponame');
+//enter your repository, it must be a repository where you have pull rights, and it needs to have CI incorporated.
+var repo = client.repo(config.repo);
 
 
 var lights = function(status){
 
-//create two instances of LEDs and toggle	
+//create two instances of LEDs and toggle
 var red = new j5.Led({pin : 9});
 var green = new j5.Led({pin : 10});
 var blue = new j5.Led({pin: 11});
@@ -36,7 +44,6 @@ if(status == 'success'){
 
 }
 
-
 var get_status = function(){
 	repo.statuses('master', function(err, status, body){
  	//do whatever you'd like with the most recent status!
@@ -49,6 +56,3 @@ var get_status = function(){
 board.on('ready', function (){
 	setInterval(get_status, 500);
 });
-
-
-
